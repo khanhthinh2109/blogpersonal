c29bfee8d506590bb626ebba24946c9b0da266fb
khanh thinh <luongkhanhthinh33@gmail.com>
2025-12-26 12:39:05 +0700

    Update blog content

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,8 +30,12 @@ const Index = () => {
 
           {/* Main Title */}
           <h1 className="font-mono text-4xl md:text-6xl lg:text-7xl font-bold mb-6 animate-fade-in" style={{ animationDelay: '100ms' }}>
-            <span className="text-foreground">Xin chào, Lương Khánh Thịnh</span>
-            <span className="text-gradient">Developer</span>
+            <span className="text-foreground">
+    Xin chào, tôi là Lương Khánh Thịnh
+  </span>
+  <span className="mx-4 text-gradient">
+    Developer
+  </span>
           </h1>
 
           {/* Subtitle */}
